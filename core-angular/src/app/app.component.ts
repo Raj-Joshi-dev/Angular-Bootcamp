198f732e20c86a64d21fe5e8fe589b4bc53481fd
Joshi <raj.joshi@siemens-healthineers.com>
2020-09-29 17:54:23 +0200

    Section 4: Core Foundation of Angular Apps

---
+++ core-angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line: component-selector
+  selector: 'my-app-main',
+  template: `<h1>Hey Angular</h1>`,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {}
